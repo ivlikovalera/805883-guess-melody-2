79f7a4ddd01089c380c6db64c85353a7a70dc9a3
Valeriya Litvinenko <ivlikovalera.ph@gmail.com>
2019-10-28 14:43:50 +0700

    Добавляет моки

---
+++ src/mocks/questions.js
@@ -0,0 +1,45 @@
+export const questions = [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `pop`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `jazz`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+    ],
+  },
+  {
+    type: `singer`,
+    song: {
+      singer: `Sting`,
+      src: `https://soundtimes.ru/images/mp3/Shape%20Of%20My%20Heart.mp3`,
+    },
+    answers: [
+      {
+        picture: `http://placehold.it/134x134`,
+        singer: `Sting`,
+      },
+      {
+        picture: `http://placehold.it/134x134`,
+        singer: `Eminem`,
+      },
+      {
+        picture: `http://placehold.it/134x134`,
+        singer: `Pink`,
+      },
+    ],
+  },
+];
